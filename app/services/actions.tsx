2066690ea324f70a22c40500224ab1ebb89ffae8
sukaarnyana <sukagood@gmail.com>
2025-12-25 21:45:35 +0800

    lupa update

---
+++ app/services/actions.tsx
@@ -0,0 +1,11 @@
+"use server"
+import { prisma } from "@/lib/db";
+
+export async function createService(formData) {
+    await prisma.service.create({
+        data: {
+            name: formData.get("serv_name"),
+            price: Number(formData.get("serv_price")),
+        }
+    })
+}
